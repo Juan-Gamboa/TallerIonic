75f1678da8928d92059d99a9997c0c65a9aab35f
NayarithRoa <nroa@uniboyaca.edu.co>
2022-11-15 08:11:13 -0500

    ajustepersona

--- IonicApi/src/app/pages/persona/persona.page.ts
+++ IonicApi/src/app/pages/persona/persona.page.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { GetapiService } from 'src/app/Servicios/getapi.service';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.page.html',
-  styleUrls: ['./inicio.page.scss'],
+  selector: 'app-persona',
+  templateUrl: './persona.page.html',
+  styleUrls: ['./persona.page.scss'],
 })
-export class InicioPage implements OnInit {
+export class PersonaPage implements OnInit {
 getdata:any[]=[];
   constructor(public _services:GetapiService) {
     this._services.getdata<any[]>("").subscribe(data =>
